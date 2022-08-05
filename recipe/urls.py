1bcd62ae7399cad06b38b7960de5883ccd9c03d9
Caroline Harte <caroline.harte@yahoo.com>
2022-08-05 17:58:23 +0000

    Added navbar

--- recipe/urls.py
+++ recipe/urls.py
@@ -4,5 +4,10 @@ from . import views
 
 urlpatterns = [
     path('', views.MainPage.as_view(), name='main'),
-    path('index', views.Index.as_view(), name='home'),
+    path('index/', views.Index.as_view(), name='home'),
+    path('about/', views.About.as_view(), name='about'),
+    path('recipelist/', views.RecipesList.as_view(), name='recipes'),
+    path('myrecipes/', views.MyRecipes.as_view(), name='myrecipes'),
+    
+
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -1,5 +1,6 @@
+from django.shortcuts import render, get_object_or_404, reverse
 from django.shortcuts import render
-from django.views import generic
+from django.views import generic, View
 from django.views.generic import TemplateView, CreateView
 from .forms import AgeCheck
 from django.urls import reverse_lazy
@@ -15,9 +16,29 @@ class AgeCheckView(CreateView):
     """Displays age check on main page"""
     form_class = AgeCheck
     sucess_url = reverse_lazy('main')
-    template_name = 'recipe/main.html'
+    template_name = 'main.html'
     
 
-class Index(generic.TemplateView):
+class Index(generic.ListView):
     """Displays home page for site"""
+    model = Recipe
     template_name = 'index.html'
+    
+
+class About(generic.TemplateView):
+    """
+    Displays information about the site
+    """
+    template_name = 'about.html'    
+
+
+class RecipesList(generic.ListView):
+    """Displays view of all recipes """
+    model = Recipe
+    template_name = 'recipes.html'
+
+
+class MyRecipes(generic.ListView):
+    """Displays logged in Users Recipes"""
+    model = Recipe
+    template_name = 'my recipes.html'
